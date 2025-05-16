71334eff0eb91874229366c7d0ad6527d945a985
HAMEL Vincent <130774386+ITvenn@users.noreply.github.com>
2025-05-16 12:13:03 +0200

    Update contact_me.js

--- mail/contact_me.js
+++ mail/contact_me.js
@@ -1,43 +1,18 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-  <meta charset="UTF-8">
-  <title>Formulaire de contact</title>
-</head>
-<body>
-  <form id="contact-form">
-    <label for="name">Nom :</label>
-    <input type="text" name="nom" id="name" required><br>
+<!-- SDK EmailJS -->
+<script src="https://cdn.emailjs.com/dist/email.min.js"></script>
+<script>
+  (function () {
+    emailjs.init("xkV_j7aevb_AkEj5p"); // Remplacez par votre ID utilisateur EmailJS
+  })();
 
-    <label for="email">Email :</label>
-    <input type="email" name="email" id="email" required><br>
+  document.getElementById("contact-form").addEventListener("submit", function (e) {
+    e.preventDefault();
 
-    <label for="phone">Téléphone :</label>
-    <input type="text" name="téléphone" id="phone" required><br>
-
-    <label for="message">Message :</label>
-    <textarea name="message" id="message" required></textarea><br>
-
-    <button type="submit">Send</button>
-  </form>
-
-  <!-- SDK EmailJS -->
-  <script src="https://cdn.emailjs.com/dist/email.min.js"></script>
-  <script>
-    (function () {
-      emailjs.init("xkV_j7aevb_AkEj5p"); // Remplacez par votre ID utilisateur EmailJS
-    })();
-
-    document.getElementById("contact-form").addEventListener("submit", function (e) {
-      e.preventDefault();
-
-      emailjs.sendForm("service_3hfkd24", "template_n4teg6d", this)
-        .then(function () {
-          alert("Message envoyé !");
-        }, function (error) {
-          alert("Erreur lors de l'envoi : " + JSON.stringify(error));
-        });
-    });
-  </script>
-</body>
-</html>
+    emailjs.sendForm("service_3hfkd24", "template_n4teg6d", this)
+      .then(function () {
+        alert("Message envoyé !");
+      }, function (error) {
+        alert("Erreur lors de l'envoi : " + JSON.stringify(error));
+      });
+  });
+</script>
